5b7ebe73cfb5917bba80d1d690c9edd34c6119c0
Fredw1999 <122766765+Fredw1999@users.noreply.github.com>
2023-05-07 18:55:18 -0400

    R

--- data/interactive map.R
+++ data/interactive map.R
@@ -4,22 +4,47 @@ library(sf)
 library(dplyr)
 
 # 读取数据
-#pop_density_data <- read.csv("path_to_population_density_data.csv")
-forest_coverage_data <- read.csv("yearly_forest_loss.csv")
+pop_density_data <- read.csv("net_migrants.csv")
+forest_coverage_data <- read.csv("yearly_forest_loss_density.csv")
 #factory_data <- read.csv("path_to_oil_and_palm_factory_data.csv")
 #language_data
 # 读取各省的边界数据
-province_shapefiles <- list.files("shp_files", pattern = "\\.shp$", full.names = TRUE)
-province_boundaries <- lapply(province_shapefiles, function(x) {
-  st_read(x)
-})
+# 读入8个省份的边界数据
+Aceh_boundary <- st_read("/Users/weijiahao/Dropbox/PC/Desktop/统计作业/5293/AP-J-S-Proj/data/shp_files/Aceh_boundary.shp")
+Bengkulu_boundary <- st_read("/Users/weijiahao/Dropbox/PC/Desktop/统计作业/5293/AP-J-S-Proj/data/shp_files/Bengkulu_boundary.shp")
+Jambi_boundary <- st_read("/Users/weijiahao/Dropbox/PC/Desktop/统计作业/5293/AP-J-S-Proj/data/shp_files/Jambi_boundary.shp")
+Lampung_boundary <- st_read("/Users/weijiahao/Dropbox/PC/Desktop/统计作业/5293/AP-J-S-Proj/data/shp_files/Lampung_boundary.shp")
+Riau_boundary <- st_read("/Users/weijiahao/Dropbox/PC/Desktop/统计作业/5293/AP-J-S-Proj/data/shp_files/Riau_boundary.shp")
+SumateraBarat_boundary <- st_read("/Users/weijiahao/Dropbox/PC/Desktop/统计作业/5293/AP-J-S-Proj/data/shp_files/SumateraBarat_boundary.shp")
+SumateraSelatan_boundary <- st_read("/Users/weijiahao/Dropbox/PC/Desktop/统计作业/5293/AP-J-S-Proj/data/shp_files/SumateraSelatan_boundary.shp")
+SumateraUtara_boundary <- st_read("/Users/weijiahao/Dropbox/PC/Desktop/统计作业/5293/AP-J-S-Proj/data/shp_files/SumateraUtara_boundary.shp")
 
-# 合并省边界数据
-all_provinces <- do.call(rbind, province_boundaries)
+# 新建数据框，包含所有省份的名称
+all_provinces <- data.frame(province_name = c("Aceh", "Bengkulu", "Jambi", "Lampung", 
+                                              "Riau", "SumateraBarat", "SumateraSelatan", "SumateraUtara"))
+
+# 将所有省份的名称与边界数据合并
+all_provinces_boundary <- all_provinces %>% 
+  left_join(Aceh_boundary, by = c("province_name" = "PROVINSI"), all = TRUE, fill = list(geometry = st_sfc()))
+all_provinces_boundary <- all_provinces_boundary %>% 
+  left_join(Bengkulu_boundary, by = c("province_name" = "PROVINSI"), all = TRUE, fill = list(geometry = st_sfc()))
+all_provinces_boundary <- all_provinces_boundary %>% 
+  left_join(Jambi_boundary, by = c("province_name" = "PROVINSI"), all = TRUE, fill = list(geometry = st_sfc()))
+all_provinces_boundary <- all_provinces_boundary %>% 
+  left_join(Lampung_boundary, by = c("province_name" = "PROVINSI"), all = TRUE, fill = list(geometry = st_sfc()))
+all_provinces_boundary <- all_provinces_boundary %>% 
+  left_join(Riau_boundary, by = c("province_name" = "PROVINSI"), all = TRUE, fill = list(geometry = st_sfc()))
+all_provinces_boundary <- all_provinces_boundary %>% 
+  left_join(SumateraBarat_boundary, by = c("province_name" = "PROVINSI"), all = TRUE, fill = list(geometry = st_sfc()))
+all_provinces_boundary <- all_provinces_boundary %>% 
+  left_join(SumateraSelatan_boundary, by = c("province_name" = "PROVINSI"), all = TRUE, fill = list(geometry = st_sfc()))
+all_provinces_boundary <- all_provinces_boundary %>% 
+  left_join(SumateraUtara_boundary, by = c("province_name" = "PROVINSI"), all = TRUE, fill = list(geometry = st_sfc()))
 
 # 根据需要，将 province_name 替换为你的 CSV 文件中的省份名称列
 pop_density_data <- left_join(all_provinces, pop_density_data, by = "province_name")
 forest_coverage_data <- left_join(all_provinces, forest_coverage_data, by = "province_name")
+
 
 ui <- fluidPage(
   titlePanel("Sumatra"),
